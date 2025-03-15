3c6a6b0e5aab719dc0bba82935d178da63b22570
Solovey1408 <songfromredstone@gmail.com>
2025-03-15 13:09:52 +0300

    ДЗ: Создание класса и метода

--- HW №32 Creating a class and method.py
+++ HW №32 Creating a class and method.py
@@ -5,19 +5,28 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 class Test:
 
+    base_url = 'https://www.saucedemo.com/'
+    window_width = 1920
+    window_height = 1080
+
+
     def __init__(self):
+
+        self._init_driver()
+
+
+    def _init_driver(self):
         self.options = webdriver.ChromeOptions()
         self.options.add_experimental_option("detach", True)
         self.driver = webdriver.Chrome(
             options=self.options,
             service=ChromeService(ChromeDriverManager().install()))
-        self.driver.set_window_size(1920, 1080)
+        self.driver.set_window_size(self.window_width, self.window_height)
 
-    #Метод
+
     def test_select_product(self):
 
-        base_url = 'https://www.saucedemo.com/'
-        self.driver.get(base_url)
+        self.driver.get(self.base_url)
 
 
 start_test = Test()
